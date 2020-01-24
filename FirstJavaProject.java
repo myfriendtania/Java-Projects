0735cfc97c4741f3ea67c8c8eb9142f3991a30e2
Tania <20519161+thetalentedleo@users.noreply.github.com>
2020-01-24 12:07:34 -0600

    Create FirstJavaProject.java

---
+++ FirstJavaProject.java
@@ -0,0 +1,17 @@
+/*
+ * Tania Torres - 2606825
+ * El Centro - COSC 1436
+ * Assignment 1 - Week 1 - SPR 2020
+ */
+
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello my name is Tania Torres!");
+		System.out.println("This is my first Java Program!");
+
+	}
+
+}
