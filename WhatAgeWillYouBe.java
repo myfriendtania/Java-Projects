3939b29aaef0f0ec585d4eeabf9be08ab53d5166
Tania <20519161+thetalentedleo@users.noreply.github.com>
2020-02-04 18:48:42 -0600

    Create WhatAgeWillYouBe.java

---
+++ WhatAgeWillYouBe.java
@@ -0,0 +1,22 @@
+
+public class whatage2030 {
+
+	public static void main(String[] args) {
+			
+			String student = "Tania Torres";
+			int year2030 = 2030;
+			int currentYear = 2020;
+			int age = 27;
+			int futureAge = age + year2030 - currentYear;
+			
+			System.out.print(student);
+			System.out.print(" will be age "); 
+			System.out.print(futureAge);
+			System.out.print(" in the year ");
+			System.out.print(year2030);
+			System.out.print(".");
+			
+		
+		}
+
+	}
