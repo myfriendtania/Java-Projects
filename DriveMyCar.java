d2dabdda10093dcdb9509cb22e446063decde649
Tania <20519161+thetalentedleo@users.noreply.github.com>
2020-02-15 20:37:37 -0600

    Create DriveMyCar.java

---
+++ DriveMyCar.java
@@ -0,0 +1,35 @@
+public class DriveMyCar {
+
+	public static void main(String[] args) {
+		
+		instructions();
+		stepOne();
+		stepTwo();
+		stepThree();
+		stepFour();
+		stepFive();
+		stepSix();
+	}	
+	public static void instructions() {
+		System.out.println("Tasks to operate a vehicle:");
+	}
+	public static void stepOne() {
+		System.out.println("1. Find your car");
+	}
+	public static void stepTwo() {
+		System.out.println("2. Unlock your car");	
+	}
+	public static void stepThree() {
+		System.out.println("3. Start car's engine");
+	}
+	public static void stepFour() {
+		System.out.println("4. Strap Seat belt and check mirrors");
+	}
+	public static void stepFive() {
+		System.out.println("5. Give address to GPS");
+	}
+	public static void stepSix() {
+			System.out.println("6. Drive to destination...");
+	}
+
+}
